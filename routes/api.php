461b249e29983e957e883520a338e1c022a8adb8
Ronal Gorba Timothy <rg.timothy@icloud.com>
2021-08-09 22:16:23 +0700

    allow CORS header method

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: Authorization, Content-Type');
 
 /*
